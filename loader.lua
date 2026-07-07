666892954aa1e7723c5a2d25758343c3d0572795
NovaHubRBLX <ejayrosetebiz@gmail.com>
2026-07-07 19:59:10 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,24 +3,15 @@ local currentId = game.PlaceId
 local game1 = 84133819568736 
 local game2 = 97598239454123 
 
-if currentId == game1 then
-   
+if currentId == game1 then 
     local success, result = pcall(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/hidenpaint.lua"))()
     end)
-    
-    if not success then
-        warn("Failed to load script: " .. tostring(result))
-    end
-    
+   
 elseif currentId == game2 then
     local success, result = pcall(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/gag2.lua"))()
     end)
-    
-    if not success then
-        warn("Failed to load script: " .. tostring(result))
-    end
     
 else
     print("Current PlaceId: " .. tostring(currentId))
