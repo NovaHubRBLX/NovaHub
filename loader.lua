2129ae12446fab41049b332b89715f8884ee8c41
NovaHubRBLX <ejayrosetebiz@gmail.com>
2026-07-07 19:48:17 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,10 @@
--- Change GameId to PlaceId
 local currentId = game.PlaceId
 
 local game1 = 84133819568736 
 local game2 = 97598239454123 
 
 if currentId == game1 then
-    -- It is best practice to wrap loadstring in a pcall to catch errors
+   
     local success, result = pcall(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/hidenpaint.lua"))()
     end)
@@ -15,7 +14,14 @@ if currentId == game1 then
     end
     
 elseif currentId == game2 then
-    print("Script running for Game 2")   
+    local success, result = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/gag2.lua"))()
+    end)
+    
+    if not success then
+        warn("Failed to load script: " .. tostring(result))
+    end
+    
 else
     print("Current PlaceId: " .. tostring(currentId))
     print("This game is not currently supported.")
