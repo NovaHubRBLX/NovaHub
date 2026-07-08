072e7878b4806c06a36f510f4b08c9d8426d2a31
NovaHubRBLX <ejayrosetebiz@gmail.com>
2026-07-08 17:45:46 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,9 @@
 local currentId = game.PlaceId
 
 local game1 = 84133819568736 
-local game2 = 97598239454123 
+local game2 = 97598239454123
+local game3 = 109983668079237 
+local game4 = 85245205758607 
 
 if currentId == game1 then 
     local success, result = pcall(function()
@@ -13,6 +15,16 @@ elseif currentId == game2 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/gag2.lua"))()
     end)
     
+elseif currentId == game3 then
+    local success, result = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/sab.lua"))()
+    end)
+    
+elseif currentId == game4 then
+    local success, result = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/paintorseek.lua"))()
+    end)
+  
 else
     print("Current PlaceId: " .. tostring(currentId))
     print("This game is not currently supported.")
