55c96282c63c288b6b5021ae53346e16321306c4
NovaHubRBLX <ejayrosetebiz@gmail.com>
2026-07-09 12:49:54 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,7 +3,8 @@ local currentId = game.PlaceId
 local game1 = 84133819568736 
 local game2 = 97598239454123
 local game3 = 109983668079237 
-local game4 = 85245205758607 
+local game4 = 85245205758607
+local game5 = 79268393072444 
 
 if currentId == game1 then 
     local success, result = pcall(function()
@@ -23,6 +24,10 @@ elseif currentId == game3 then
 elseif currentId == game4 then
     local success, result = pcall(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/paintorseek.lua"))()
+    end)
+elseif currentId == game4 then
+    local success, result = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/selllemon.lua"))()
     end)
   
 else
