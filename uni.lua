6dccf6e0dc76fab28f12c0adddd15990bf2e120b
NovaHubRBLX <ejayrosetebiz@gmail.com>
2026-07-15 01:27:39 +0800

    Update uni.lua

--- uni.lua
+++ uni.lua
@@ -1,7 +1,7 @@
-local TARGET_UNIVERSE_ID = 10267363348
+local GAME1 = 10267363348
 local SCRIPT_URL = "https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/drain.lua"
 
-if game.GameId == TARGET_UNIVERSE_ID then
+if game.GameId == GAME1 then
    
     
     local success, result = pcall(function()
