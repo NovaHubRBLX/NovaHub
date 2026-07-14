b3aafd23f040cd195c3a4cc99648e440f421c343
NovaHubRBLX <ejayrosetebiz@gmail.com>
2026-07-15 00:40:43 +0800

    Update uni.lua

--- uni.lua
+++ uni.lua
@@ -3,6 +3,7 @@ local uni = 138381251771774
 if game.GameId == uni then
 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/drain.lua"))()
+    print("loaded")
 else
     print("This script is not intended for this game universe.")
 end
