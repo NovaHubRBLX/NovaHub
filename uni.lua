086e994b34174ce0fef3b62815650a66a9d98bb5
NovaHubRBLX <ejayrosetebiz@gmail.com>
2026-07-18 16:44:41 +0800

    Update uni.lua

--- uni.lua
+++ uni.lua
@@ -1,15 +1,20 @@
 local GAME1 = 10267363348
-local SCRIPT_URL = "https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/drain.lua"
+local SCRIPT_URL1 = "https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/drain.lua"
+local GAME2 = 99702578544768
+local SCRIPT_URL2 = "https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/fishbaitEvent.lua"
 
 if game.GameId == GAME1 then
    
-    
     local success, result = pcall(function()
-        return loadstring(game:HttpGet(SCRIPT_URL))()
+        return loadstring(game:HttpGet(SCRIPT_URL1))()
     end)
-  
+ elseif  game.GameId == GAME2 then
+   local success, result = pcall(function()
+        return loadstring(game:HttpGet(SCRIPT_URL2))()
+    end)
 else
      local success, result = pcall(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/notsupported.lua"))()
     end)
 end
+
