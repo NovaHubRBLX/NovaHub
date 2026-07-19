d3641ea0a416a00ea1d4b4bec5c8e303c48d1063
NovaHubRBLX <ejayrosetebiz@gmail.com>
2026-07-19 22:27:10 +0800

    Update uni.lua

--- uni.lua
+++ uni.lua
@@ -3,16 +3,21 @@ local GAME1 = 10267363348
 local SCRIPT_URL1 = "https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/drain.lua"
 local GAME2 = 9330616906
 local SCRIPT_URL2 = "https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/fishbaitEvent.lua"
+local GAME3 = 10131390815
+local SCRIPT_URL3 = "https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/throwacoin.lua"
 
 if game.GameId == GAME1 then
-   
     local success, result = pcall(function()
         return loadstring(game:HttpGet(SCRIPT_URL1))()
     end)
    
- elseif  game.GameId == GAME2 then
+elseif  game.GameId == GAME2 then
    local success, result = pcall(function()
         return loadstring(game:HttpGet(SCRIPT_URL2))()
+    end)
+elseif  game.GameId == GAME3 then
+   local success, result = pcall(function()
+        return loadstring(game:HttpGet(SCRIPT_URL3))()
     end)
    
 else
