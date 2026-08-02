e3858795f9066b86c52c28961a9dc1b6f37e38ac
NovaHubRBLX <ejayrosetebiz@gmail.com>
2026-08-02 11:40:10 +0800

    Update uni.lua

--- uni.lua
+++ uni.lua
@@ -2,7 +2,7 @@ print("Nova Hub Loaded Successfully")
 local GAME1 = 10267363348
 local SCRIPT_URL1 = "https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/drain.lua"
 local GAME2 = 9330616906
-local SCRIPT_URL2 = "https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/fishbaitEvent.lua"
+local SCRIPT_URL2 = "https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/fishbait.lua"
 local GAME3 = 10131390815
 local SCRIPT_URL3 = "https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/throwacoin.lua"
 local GAME4 = 10148434559
